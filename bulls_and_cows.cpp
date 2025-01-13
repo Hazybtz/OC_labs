f15da37cdbfbe8d770422799ad9f3aeb2ca84456
Artyom <hazybtz@gmail.com>
2025-01-13 07:54:59 +0500

    add func isNum

--- bulls_and_cows.cpp
+++ bulls_and_cows.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <ctime>
 #include <windows.h>
 using namespace std;
@@ -9,29 +11,48 @@ string generateRandomNum(size_t len)
     string number;
     while (number.length() < len) 
     {
-        
         char digit = '0' + rand() % 10;
         if (!(number.find(digit) != string::npos || (number.length() == 0 && digit == '0'))) 
         {
             number += digit;
         }
-        
     }
     return number;
 }
+
+bool isNum(string player, vector<char> data)
+{
+    for (size_t i = 0; i < player.length(); i++) 
+    {
+        auto res = find(begin(data), end(data), player[i]);
+        if (res == end(data)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() 
 {
     setlocale(LC_ALL, "RU");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    const vector<char> data = {'0','1','2','3','4','5','6','7','8','9'};
+    const size_t len = 4; 
 
     srand(time(0));
-    const size_t len = 4; 
     string pc = generateRandomNum(len) ;
     string player;
-
-    cout<<pc;
-
+    cout << "Введите предполагаемое число: ";
+    cin >> player;
+    while(!(isNum(player, data)) || player.length() != len)
+    {
+        cout << "ошибка ввода! Попробуйте еще раз.\n";
+        cout << "Введите предполагаемое четырехзначное число: ";
+        cin >> player;
+    }
+    
+    
 
     return 0;
 }
