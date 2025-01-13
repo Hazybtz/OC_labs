5866149d5d81d5b5edf72eda908e2aabff860aa8
Artyom <hazybtz@gmail.com>
2025-01-13 08:15:48 +0500

    add func countBullsAndCows and end logic of game

--- bulls_and_cows.cpp
+++ bulls_and_cows.cpp
@@ -32,26 +32,61 @@ bool isNum(string player, vector<char> data)
     return true;
 }
 
+void countBullsAndCows(const string& pc, const string& player, int& bulls, int& cows) 
+{
+    bulls = 0;
+    cows = 0;
+    for (size_t i = 0; i < pc.length(); i++) 
+    {
+        if (player[i] == pc[i]) 
+        {
+            bulls++;
+        } 
+        else if (pc.find(player[i]) != string::npos) 
+        {
+            cows++;
+        }
+    }
+}
+
 int main() 
 {
     setlocale(LC_ALL, "RU");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    srand(time(0));
+    
     const vector<char> data = {'0','1','2','3','4','5','6','7','8','9'};
     const size_t len = 4; 
 
-    srand(time(0));
     string pc = generateRandomNum(len) ;
     string player;
-    cout << "Введите предполагаемое число: ";
-    cin >> player;
-    while(!(isNum(player, data)) || player.length() != len)
+    while(true)
     {
-        cout << "ошибка ввода! Попробуйте еще раз.\n";
-        cout << "Введите предполагаемое четырехзначное число: ";
+        cout << pc;
+        cout << "Введите предполагаемое число: ";
         cin >> player;
+
+        while(!(isNum(player, data)) || player.length() != len)
+        {
+            cout << "ошибка ввода! Попробуйте еще раз.\n";
+            cout << "Введите предполагаемое четырехзначное число: ";
+            cin >> player;
+        }
+
+        int bulls = 0, cows = 0;
+        countBullsAndCows(pc, player, bulls, cows);
+        
+        if (bulls == len) 
+        {
+            cout << "Поздравляем! Вы угадали число " << pc << endl;
+            break;
+        } 
+        else 
+        {
+            cout << "Быки: " << bulls << ", Коровы: " << cows << "\n";
+        }
     }
-    
     
 
     return 0;
